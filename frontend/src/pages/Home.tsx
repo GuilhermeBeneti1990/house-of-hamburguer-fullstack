27724cbf09df12e1ede2f81b81a12b97f941d1a2
Guilherme Beneti <guilhermebeneti@icloud.com>
2026-04-09 12:21:58 -0300

    feat: category filter implemented in frontend

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [category, setCategory] = useState("Hamburguer");
+
+  const handleChangeCategory = (newCategory: string) => {
+    setCategory(newCategory);
+  };
+
+  const getCategoryClass = (categoryName: string) => {
+    const elementSelected =
+      "md:text-md flex h-7 w-24 cursor-pointer items-center justify-center rounded-md border-1 border-[#F2DAAC] bg-[#F2DAAC] text-sm font-bold text-[#161410] md:h-9 md:w-32";
+    const elementNotSelected =
+      "md:text-md hover:bg flex h-7 w-24 cursor-pointer items-center justify-center rounded-md border-1 border-[#F2DAAC] bg-[#161410] text-sm font-bold text-[#F2DAAC] hover:bg-[#F2DAAC] hover:text-[#161410] md:h-9 md:w-32";
+
+    if (categoryName === category) {
+      return elementSelected;
+    } else {
+      return elementNotSelected;
+    }
+  };
+
   return (
-    <div className="text-white">
-      <h1>Cardápio</h1>
+    <div className="mx-auto w-full px-3 md:w-[737px] md:px-0">
+      <div className="my-1 flex gap-2 md:my-3">
+        <div
+          className={getCategoryClass("Hamburguer")}
+          onClick={() => handleChangeCategory("Hamburguer")}
+        >
+          Hamburguer
+        </div>
+        <div
+          className={getCategoryClass("Bebidas")}
+          onClick={() => handleChangeCategory("Bebidas")}
+        >
+          Bebidas
+        </div>
+        <div
+          className={getCategoryClass("Porções")}
+          onClick={() => handleChangeCategory("Porções")}
+        >
+          Porções
+        </div>
+      </div>
     </div>
   );
 };
